b04c3d75bafd75f93fb79b6f65745761cb2e1bc8
alexpaine04 <alexanderpaine2024@u.northwestern.edu>
2024-01-31 23:04:48 -0600

    further adf analysis

--- midterm/04_stationary_analysis.R
+++ midterm/04_stationary_analysis.R
@@ -6,8 +6,12 @@ library(mice)
 library(caret)
 library(stats)
 library(urca)
+library(tseries)
 
 load(file = "data/covid_clean.rda")
+
+# don't have this rda on my local so added csv read - Alex
+covid_clean <- read_csv("covid_clean_lags.csv")
 
 covid_sum <- covid_clean %>% 
   filter(
@@ -26,3 +30,13 @@ variable <- ts_data[,2]
 # Augmented Dickey-Fuller test
 adf_test <- ur.df(variable, type = "trend", lags = 10)
 summary(adf_test)
+
+# alternate method for Augmented Dickey-Fuller test. Was slightly confused by
+# the summary results above
+adf.test(covid_clean$new_cases)
+
+
+
+
+
+
