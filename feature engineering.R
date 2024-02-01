b38befa071d47dcfaef791f9ba45dd685930af2d
alexpaine04 <alexanderpaine2024@u.northwestern.edu>
2024-01-31 21:07:01 -0600

    added features: lags, rolling windows, datetime stats

--- feature engineering.R
+++ feature engineering.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(tidymodels)
 library(lubridate)
+library(tibbletime)
 
 data <- read.csv("data/covid_clean.csv")
 
@@ -29,6 +30,45 @@ fdata <- data %>%
 
 # class check
 class(fdata$date)
+
+#na's per column
+nas <- tibble(variable = names(colMeans(is.na(fdata))), pct_missing = colMeans(is.na(fdata)))
+
+nas %>% 
+  arrange(-pct_missing) %>% 
+  print(n=70)
+
+
+# creating lag variables, rolling windows, and date time features
+# first need rolling functions
+mean_roll_6 <- rollify(mean, window = 6)
+mean_roll_12 <- rollify(mean, window = 12)
+
+gdata <- fdata %>% 
+  group_by(iso_code) %>% 
+  #lags
+  mutate(new_cases_2week_lag = lag(new_cases, n=2, default=NA),
+         new_cases_6week_lag = lag(new_cases, n=6, default=NA),
+         new_deaths_2week_lag = lag(new_deaths, n=2, default=NA),
+         new_deaths_6week_lag = lag(new_deaths, n=6, default=NA)) %>% 
+  relocate(new_cases_2week_lag,.after = new_cases) %>% 
+  relocate(new_cases_6week_lag,.after = new_cases_2week_lag) %>% 
+  relocate(new_deaths_2week_lag,.after = new_deaths) %>% 
+  relocate(new_deaths_6week_lag,.after = new_deaths_2week_lag) %>% 
+  # rolling statistics
+  mutate(new_cases_6week_roll = mean_roll_6(new_cases),
+         new_cases_12week_roll = mean_roll_12(new_cases),
+         new_deaths_6week_roll = mean_roll_6(new_deaths),
+         new_deaths_12week_roll = mean_roll_12(new_deaths)) %>% 
+  relocate(new_cases_6week_roll,.after = new_cases_6week_lag) %>% 
+  relocate(new_cases_12week_roll,.after = new_cases_6week_roll) %>% 
+  relocate(new_deaths_6week_roll,.after = new_deaths_6week_lag) %>% 
+  relocate(new_deaths_12week_roll,.after = new_deaths_6week_roll) %>% 
+  # date time features
+  mutate(month = lubridate::month(date),
+         week = lubridate::week(date))
+
+write_csv(gdata, file = "data/covid_clean_lags.csv")
 
 
 
