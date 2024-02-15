2f64803c79deaf807ebdad63e2408337c5f28d2f
Donald Stricklin <DonaldStricklin2023@u.northwestern.edu>
2024-02-15 15:32:12 -0600

    commit

--- midterm/02_seasonal_decomposition.R
+++ midterm/02_seasonal_decomposition.R
@@ -5,30 +5,37 @@ library(forecast)
 library(mice)
 library(caret)
 library(stats)
+library(lubridate)
 
-load(file = "data/covid_clean.rda")
+# Australia ----
+Australia_uni <- read_csv("country_data/univariate/Australia_uni.csv")
 
-covid_sum <- covid_clean %>% 
-  filter(
-    date < "2024-01-01"
-  ) %>% 
-  group_by(date) %>% 
-  select(date, new_cases) %>% 
-  summarise(
-    num_cases = sum(new_cases, na.rm = T)
-  )
+aus_ts <- ts(Australia_uni$new_cases, 
+               freq=365.25/7, 
+               start=decimal_date(ymd("2020-01-05")))
 
-ts_data <- ts(covid_sum, frequency = 52)
+aus_decomp <- decompose(aus_ts)
+plot(aus_decomp) + title(outer = F, sub = "Australia")
 
-decomposition_result <- decompose(ts_data)
+# Ireland ----
+Ireland_uni <- read_csv("country_data/univariate/Ireland_uni.csv")
 
-trend_component <- decomposition_result$trend
-seasonal_component <- decomposition_result$seasonal
-residual_component <- decomposition_result$residual # Not getting any results for some reason ???
+ire_ts <- ts(Ireland_uni$new_cases, 
+               freq=52, 
+               start=decimal_date(ymd("2020-01-05")))
 
-# Plot the components
-plot(trend_component, main = "Trend Component")
-plot(seasonal_component, main = "Seasonal Component")
-plot(residual_component, main = "Residual Component")
+ire_decomp <- decompose(ire_ts)
+plot(ire_decomp) + title(sub = "Ireland")
+
+
+# Mexico ----
+Mexico_uni <- read_csv("country_data/univariate/Mexico_uni.csv")
+
+mex_ts <- ts(Mexico_uni$new_cases, 
+             freq=52, 
+             start=decimal_date(ymd("2020-01-05")))
+
+mex_decomp <- decompose(mex_ts)
+plot(mex_decomp) + title(sub = "Mexico")
 
 
