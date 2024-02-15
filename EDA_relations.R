f363fc4801bf642bbabb53897b72fda6e812eab6
alexpaine04 <alexanderpaine2024@u.northwestern.edu>
2024-02-15 15:48:16 -0600

    setting up ARIMA

--- EDA_relations.R
+++ EDA_relations.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(scales)
 
 data <- read.csv("data/covid_cleaner.csv")
 
@@ -6,3 +7,23 @@ mdata <- read.csv("data/covid_clean_lags.csv")
 
 ggplot(mdata, aes(x = month, y = new_cases)) +
   geom_col()
+
+ggplot(mdata, aes(x = week, y = new_cases)) + 
+  geom_col(fill="purple") +
+  ggtitle("New Cases by Week") +
+  theme_minimal()
+
+#china data
+bigcountries <- mdata %>% 
+  filter(iso_code %in% c("CHN", "RUS", "USA", "IND", "BRA")) %>% 
+  mutate(datee = lubridate::ymd(date)) %>%
+  select(-date) %>% 
+  mutate(date = datee) %>% 
+  select(-datee) %>% 
+  relocate(date)
+
+ggplot(bigcountries, aes(x = date, y = new_cases, group = iso_code, color = location)) +
+  geom_line() +
+  scale_x_date(date_breaks = "years", date_labels = "20%y") +
+  theme_minimal()
+  
